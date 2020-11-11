e1ec38fdb8b045e29fec8f95179682504e3e711f
Goran Ponjevic <45714339+minnuss@users.noreply.github.com>
2020-11-11 16:37:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const countdown = document.querySelector('#countdown');
+
+do {
+    var question = Number(prompt('Enter time in minutes'));
+} while (isNaN(question));
+
+let n = question;
+let seconds = n * 60;
+
+setInterval(timer, 1000);
+
+function timer() {
+
+    if (seconds === 0) {
+        let alert = document.querySelector('.alert');
+        alert.innerText = `Time's up !`;
+        alert.style.opacity = '1';
+
+        let btn = document.querySelector('.btn');
+        btn.style.opacity = '1';
+
+        btn.addEventListener('click', refreshPage);
+
+        function refreshPage() {
+            window.location.reload();
+        }
+    }
+
+    let min = Math.floor(seconds / 60);
+    let sec = seconds % 60;
+
+    sec = sec < 10 ? '0' + sec : sec;
+    min = min < 10 ? '0' + min : min;
+
+    countdown.innerHTML = `${min}:${sec}`;
+    seconds--;
+    seconds = seconds < 0 ? 0 : seconds;
+}
+
+timer();
